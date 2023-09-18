f1b59e1a846d5d1a5766c13c7bee79a8ec6181ca
lfmll <luisfernandomedinallorenti@gmail.com>
2023-09-18 10:36:26 -0400

    1.1 creacion de pagina de inicio

--- app/src/main/java/com/lfmll/unviaje/MainActivity.kt
+++ app/src/main/java/com/lfmll/unviaje/MainActivity.kt
@@ -16,31 +16,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UnViajeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            setContentView(R.layout.activity_main)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnViajeTheme {
-        Greeting("Android")
-    }
-}
